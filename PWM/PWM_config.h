d28fb2faf531fd4ff16dc2bc228ad73e460d2480
mohamed kordy <155773737+Mohamed-Kordy@users.noreply.github.com>
2024-01-08 20:01:07 +0200

    Add files via upload

---
+++ PWM/PWM_config.h
@@ -0,0 +1,21 @@
+/*
+ * PWM_config.h
+ *
+ *  Created on: Jul 9, 2023
+ *      Author: kordy
+ */
+
+#ifndef PWM_CONFIG_H_
+#define PWM_CONFIG_H_
+/*
+ * Options:
+ *        PRESCALER_1
+ *        PRESCALER_8
+ *        PRESCALER_64
+ *        PRESCALER_256
+ *        PRESCALER_1024
+ *
+ *  */
+#define PRESCALER   PRESCALER_8
+
+#endif /* PWM_CONFIG_H_ */
